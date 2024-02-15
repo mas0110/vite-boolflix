02e5b873a9106cc7e10ca0893e85644a21a7a086
mas0110 <masoudmohammed13@gmail.com>
2024-02-15 09:37:44 +0100

    voto da 1 a 5

--- src/components/CompSinglecard.vue
+++ src/components/CompSinglecard.vue
@@ -5,7 +5,9 @@ export default{
     data(){
     return{
         store,
-    }
+    } 
+},
+methods:{
 }
 }
 </script>
@@ -13,23 +15,17 @@ export default{
 <template>
 <div class="container">
     <div class="row justify-content-center">
-        <div class="card text-center col-2 m-3" style="height: 300px" v-for="(element, index) in store.FilmList">
+        <div class="card text-center col-2 m-3" style="height: 400px" v-for="(element, index) in store.FilmList">
             <figure class="">
-                <img :src="'https://image.tmdb.org/t/p/w154' + element.poster_path" alt="">
+                <img :src="'https://image.tmdb.org/t/p/w185' + element.poster_path" alt="">
             </figure>
             <h5>Titolo: {{ (element.title) ? element.original_title : element.original_name}}</h5>
             <span>Lingua originale: {{ element.original_language }}</span>
-            <span>voto: {{ element.vote_average }}</span>
+            <span>voto: {{ Math.min(5, Math.max(1, element.vote_average | number )) }}</span>
         </div>
     </div>
 </div>
 </template>
 
 <style>
-.card h6, span{
-    display: none;
-}
-.card:hover{
-
-}
 </style>
