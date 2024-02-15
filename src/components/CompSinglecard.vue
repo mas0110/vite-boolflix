9452d684d4435a4660efb33330d7bf2fe1ca9a89
mas0110 <masoudmohammed13@gmail.com>
2024-02-15 19:48:00 +0100

    stelle voto e interfaccia grafica

--- src/components/CompSinglecard.vue
+++ src/components/CompSinglecard.vue
@@ -25,8 +25,17 @@ methods:{
             default:
                 return language
         }
+    },
+    getStar(voto) {
+        let stelle = '';
+        // Itera da 0 fino a voto - 1 per aggiungere una stella per ogni unità
+        for (let i = 0; i < voto; i++) {
+          stelle += '★';
+        }
+        return stelle;
     }
-    
+
+
 }
 }
 </script>
@@ -34,22 +43,53 @@ methods:{
 <template>
 <div class="container">
     <div class="row justify-content-center">
-        <div class="card text-center col-2 m-3" style="height: 400px" v-for="(element, index) in store.FilmList">
+        <div class="card text-center col-2 m-3 border-0" style="height: 300px" v-for="(element, index) in store.FilmList">
             <figure class="">
-                <img :src="'https://image.tmdb.org/t/p/w185' + element.poster_path" alt="">
+                <img :src="'https://image.tmdb.org/t/p/w185' + element.poster_path" :alt="element.original_name">
             </figure>
-            <h5>{{ (element.title) ? element.original_title : element.original_name}}</h5>
-            <figure>
-                <span class="opacity-50">Lingua originale: </span>
-                <img :src="`https://flagsapi.com/${ getCountrycode(element.original_language).toUpperCase()}/flat/24.png`" 
-                width="24" 
-                :alt="element.original_language">
-            </figure>
-            <span>voto: {{ Math.min(5, Math.max(1, element.vote_average | number)) }}</span>
+            <div class="text p-3">
+                <h4>{{ (element.title) ? element.original_title : element.original_name}}</h4>
+                <figure>
+                    <span class="opacity-50">Lingua originale: </span>
+                    <img :src="`https://flagsapi.com/${ getCountrycode(element.original_language).toUpperCase()}/flat/24.png`" 
+                    width="24" 
+                    :alt="element.original_language">
+                </figure>
+                <span>voto:{{ getStar(Math.min(5, Math.max(1, element.vote_average))) }}</span>
+                <p class="mt-2">{{ element.overview }}</p>
+            </div>
         </div>
     </div>
 </div>
 </template>
 
-<style>
+<style lang="scss">
+.container{
+    padding-top: 90px;
+}
+header{
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+}
+.text{
+display: none;
+overflow: auto;
+}
+.card{
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+.card:hover{
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.5);
+    background-color: black;
+    figure:first-child{
+        display: none;
+
+    }
+    .text{
+    display: block;
+}
+}
 </style>
